15f8ad4673060fce02d88ea568099c7d90b8f459
priyal <priyalbhesaniya@gmail.com>
2024-10-16 09:07:37 +0530

    change in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,19 @@
-import 'package:chemlab_flutter_project/screens/LoadingPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:chemlab_flutter_project/screens/SignUpPage.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Ensures Flutter is fully initialized
-  try {
-    await Firebase.initializeApp(); // Initializes Firebase
-  } catch (e) {
-    // Handle Firebase initialization error
-    print("Firebase initialization error: $e");
-  }
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ChemLab Flutter Project',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: LoadingPage(),
+      title: 'Flutter Firebase App',
+      home: SignUpPage(),
     );
   }
 }
