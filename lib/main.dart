1345d8d12b1f48cfe20a60701b0e8f37d0fb53cc
priyal <priyalbhesaniya@gmail.com>
2024-10-16 08:50:17 +0530

    now every thing is looks good

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,31 @@
 import 'package:chemlab_flutter_project/screens/LoadingPage.dart';
- // Clean up file naming
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensures Flutter is fully initialized
-  await Firebase.initializeApp(); // Initializes Firebase
+  try {
+    await Firebase.initializeApp(); // Initializes Firebase
+  } catch (e) {
+    // Handle Firebase initialization error
+    print("Firebase initialization error: $e");
+  }
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'ChemLab Flutter Project', // It's better to give a project-specific title
+      title: 'ChemLab Flutter Project',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true, // Using Material 3 is good for modern design
+        useMaterial3: true,
       ),
-      home:  LoadingPage(), // Added const for optimization if the widget is constant
+      home: LoadingPage(),
     );
   }
 }
