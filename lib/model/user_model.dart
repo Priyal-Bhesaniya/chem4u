2e393ac19fd64d5814ff8bc5fd82affb55a26b42
priyal <priyalbhesaniya@gmail.com>
2024-10-17 13:41:49 +0530

    UserModel i m making this file to store the data in data base

---
+++ lib/model/user_model.dart
@@ -0,0 +1,14 @@
+class UserModel {
+  final String? id;
+  final String? Username;
+  final String? Password;
+  final String? Email;
+
+
+  const UserModel({
+    this.id,
+    this.Username,
+    this.Password,
+    this.Email,
+  });
+}
