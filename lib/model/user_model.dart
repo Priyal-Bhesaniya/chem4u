2ec553f3f0e391bf23895ee6eee9ad613ceb4f23
priyal <priyalbhesaniya@gmail.com>
2024-10-19 22:52:40 +0530

    now i want to add data in firestore database

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,24 +1,26 @@
 class UserModel {
-  String email;
-  String password;
-  String username;
-  String mobile;  // Add mobile number field
 
-  UserModel({
+ final String?id;
+ final String?username;
+ final String?email;
+ final String? password;
+
+
+
+  const UserModel({
+    this.id,
     required this.email,
     required this.password,
     required this.username,
-    required this.mobile,  // Include mobile number in constructor
   });
 
-  String? get id => null;
-
-  Map<String, dynamic> toJson() {
-    return {
-      'email': email,
-      'password': password,
-      'username': username,
-      'mobileNumber': mobile,  // Add toJson method for mobile number
-    };
-  }
+ 
+toJson(){
+  return {
+    'id': id,
+    'username': username,
+    'email': email,
+    'password': password,
+  };
+}
 }
