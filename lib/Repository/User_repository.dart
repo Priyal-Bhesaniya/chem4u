644b1c1bb230d2878622646310047ce1abae6bbd
priyal <priyalbhesaniya@gmail.com>
2024-10-19 23:11:32 +0530

    this page is ready

--- lib/Repository/User_repository.dart
+++ lib/Repository/User_repository.dart
@@ -12,8 +12,8 @@ class UserRepository extends GetxController{
 
   final _db = FirebaseFirestore.instance;
 
-  createUser(UserModel user){
-    _db.collection("Users").add(user.toJson()).whenComplete(
+  createUser(UserModel user) async {
+    await _db.collection("Users").add(user.toJson()).whenComplete(
       ()=> Get.snackbar("success", "Your account has been created.",
       snackPosition: SnackPosition.BOTTOM,
       backgroundColor: Colors.green.withOpacity(0.1),
